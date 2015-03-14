0026a05d73225230e21c4d3a81b105801e7ba9a1
mortenjust <morten.just@genstart.dk>
2015-03-13 22:31:53 -0700

    Initial Commit

---
+++ springanddamping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  springanddamping
+//
+//  Created by Morten Just Petersen on 3/13/15.
+//  Copyright (c) 2015 Morten Just Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
